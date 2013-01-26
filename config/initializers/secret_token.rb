d80e73e16270667728ac432ad8961e723943bb57
ryuma.tsukano <ryuma.tsukano@inte.co.jp>
2013-01-26 20:23:57 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jikken::Application.config.secret_token = 'c3dfae67f884ed0e26926ed7fe7d055fd7bc14e051cf2b8f3e60c6009998e11eadb1ba0ecb2ee9998898b68f2873472ecf10ba06f5205641aab97bd64636c813'
